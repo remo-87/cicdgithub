2afc30ccfc9f59621afbed72dd4478188f2ce836
Ricardo Martinez Osorio <ricardo.martinez@colfive.com>
2022-01-03 23:21:02 -0500

    initial

---
+++ src/test/java/com/remo/cicdgithub/CicdGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remo.cicdgithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
